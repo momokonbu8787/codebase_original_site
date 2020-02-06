e66f937faab446ca00d29e48f8c359801958b924
momokonbu8787 <momokonbu8787@gmail.com>
2020-02-06 21:01:43 +0900

    index.htmlを追加

---
+++ js/my.js
@@ -0,0 +1,80 @@
+$(function() {
+    $('#explain').hover(
+    function() {
+        $('.stripe1').fadeIn(1500);
+      },
+    function() {
+        $('.stripe1').fadeOut();
+      });
+    });
+
+$(function() {
+    $('#explain').hover(
+    function() {
+        $('.stripe2').fadeIn(3000);
+   },
+    function() {
+        $('.stripe2').fadeOut();
+      });
+    });
+
+$(function() {
+    $('#explain').hover(
+    function() {
+        $('.stress1').fadeIn(3000);
+   },
+    function() {
+            $('.stress1').fadeOut();
+      });
+    });
+$(function() {
+    $('#explain').hover(
+    function() {
+        $('.stress2').fadeIn(3000);
+   },
+    function() {
+            $('.stress2').fadeOut();
+       });
+    });
+
+$(function() {
+    $('#explain').hover(
+    function() {
+        $('.stress3').fadeIn(3000);
+   },
+        function() {
+             $('.stress3').fadeOut();
+          });
+        });
+$(function() {
+    $('#explain').hover(
+    function() {
+        $('.stress4').fadeIn(3000);
+   },
+        function() {
+             $('.stress4').fadeOut();
+        });
+    });
+
+
+ $(function() {
+     $('#modal-close1').click(function(){
+         $('#modal-content1').fadeOut();
+     });
+ });
+ $(function() {
+     $('#modal-close2').click(function(){
+         $('#modal-content2').fadeOut();
+     });
+ });
+ $(function() {
+     $('#modal-close3').click(function(){
+         $('#modal-content3').fadeOut();
+     });
+ });
+ $(function() {
+     $('#modal-close4').click(function(){
+         $('#modal-content4').fadeOut();
+     });
+ });
+
